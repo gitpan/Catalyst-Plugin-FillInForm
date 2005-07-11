1067b97127d23cc0ab72a963bcb111fe5d14c23d
Marcus Ramberg <mramberg@cpan.org>
2005-07-11 14:18:18 +0000

    Import of MRAMBERG/Catalyst-Plugin-FillInForm-0.06 from CPAN.
    gitpan-cpan-distribution: Catalyst-Plugin-FillInForm
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         MRAMBERG/Catalyst-Plugin-FillInForm-0.06.tar.gz
    gitpan-cpan-author:       MRAMBERG
    gitpan-cpan-maturity:     released

--- FillInForm.pm
+++ FillInForm.pm
@@ -4,7 +4,7 @@ use strict;
 use NEXT;
 use HTML::FillInForm;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 =head1 NAME
 
@@ -12,26 +12,53 @@ Catalyst::Plugin::FillInForm - FillInForm for Catalyst
 
 =head1 SYNOPSIS
 
-    use Catalyst 'FillInForm';
+    use Catalyst 'FillInForm'; 
+    # that's it, if Catalyst::Plugin::FormValidator is being used
 
-    $c->fillform;
+    # OR, manually:
+
+    # in MyApp.pm; assume $c->stash->data is seeded elsewhere
+    sub end : Private {
+      my ( $self, $c ) = @_;
+      $c->forward('MyApp::V::TT') unless $c->res->output;
+      $c->fillform( $c->stash->data );
+      # ....
 
 =head1 DESCRIPTION
 
-Fill forms automatically.
+Fill forms automatically, based on data from a previous HTML
+form. Typically (but not necessarily) used in conjunction with
+L<Catalyst::Plugin::FormValidator>. This module automatically
+inserts data from a previous HTML form into HTML input fields,
+textarea fields, radio buttons, checkboxes, and select
+tags. It is an instance of L<HTML::FillInForm>, which itself
+is a subclass of L<HTML::Parser>, which it uses to parse the
+HTML and insert the values into the proper form tags.
+
+The usual application is after a user submits an HTML form
+without filling out a required field, or with errors in fields
+having specified constraints. FillInForm is used to
+redisplay the HTML form with all the form elements containing
+the submitted info. FillInForm can also be used to fill forms
+with data from any source, e.g. directly from your database.
 
 =head2 EXTENDED METHODS
 
 =head3 finalize
 
-Will automatically fill in forms if the last form has missing/
-invalid fields.
+Will automatically fill in forms, based on the parameters in
+C<$c-E<gt>req-E<gt>parameters>, if the last form has missing
+or invalid fields, and if C<Catalyst::Plugin::FormValidator>
+is being used. C<finalize> is called automatically by the
+Catalyst Engine; the end user will not have to call it
+directly. (In fact, it should never be called directly by the
+end user.)
 
 =cut
 
 sub finalize {
     my $c = shift;
-    if ( $INC{'Catalyst/Plugin/FormValidator.pm'} ) {
+    if ( $c->isa('Catalyst::Plugin::FormValidator') ) {
         $c->fillform
           if $c->form->has_missing
           || $c->form->has_invalid
@@ -44,10 +71,23 @@ sub finalize {
 
 =head3 fillform
 
-Fill form based on request parameters.
+Fill a form, based on request parameters (the default) or any
+other specified data hash. You would call this manually if
+you're getting your data from some source other than the
+parameters (e.g. if you're seeding an edit form with the
+results of a database query), or if you're using some other
+validation system than C<Catalyst::Plugin::FormValidator>.
 
-    $c->fillform;
-    $c->fillform( \%parameters );
+    $c->fillform; # defaults to $c->req->parameters
+
+    # OR
+
+    $c->fillform( \%data_hash );
+
+C<fillform> must be called after an HTML template has been
+rendered. A typical way of using it is to place it immediately
+after your C<forward> call to your view class, which might be
+in a built-in C<end> action in your application class.
 
 =cut
 
@@ -65,12 +105,13 @@ sub fillform {
 
 =head1 SEE ALSO
 
-L<Catalyst>, L<HTML::FillInForm> .
+L<Catalyst>, L<Catalyst::Plugin::FormValidator>, L<HTML::FillInForm>.
 
 =head1 AUTHOR
 
 Sebastian Riedel, C<sri@cpan.org>
 Marcus Ramberg, C<mramberg@cpan.org>
+Jesse Sheidlower, C<jester@panix.com>
 
 =head1 COPYRIGHT
 
